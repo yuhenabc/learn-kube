2e16c45f7768ea64e8599dc960f86676765b9911
yuhenabc <yuhenabc@126.com>
2023-05-04 15:48:09 +0800

    add three deployments

--- shells/openminikube.sh
+++ shells/openminikube.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export MINIKUBE_IN_STYLE=false
-export HTTP_PROXY="http://192.168.21.88:7070"
-export HTTPS_PROXY="http://192.168.21.88:7070"
+export HTTP_PROXY="http://192.168.20.10:7070"
+export HTTPS_PROXY="http://192.168.20.10:7070"
 export NO_PROXY="localhost,127.0.0.1,10.96.0.0/12,192.168.59.0/24,192.168.49.0/24,192.168.39.0/24"
 
 minikube start
